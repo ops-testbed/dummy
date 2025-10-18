7c979dfcdc8de9eee285421e0d5167c0d88c243f
yoon-youngjin <dudwls143@gmail.com>
2025-10-18 20:54:33 +0900

    feature: apply ASYNC-KAFKA-APPENDER

--- api/src/main/kotlin/dev/yoon/dummy/SystemController.kt
+++ api/src/main/kotlin/dev/yoon/dummy/SystemController.kt
@@ -1,12 +1,16 @@
 package dev.yoon.dummy
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SystemController {
+    private val logger = KotlinLogging.logger {}
+
     @GetMapping("/health")
     fun health(): String {
+        logger.info { "test..!" }
         return "test..!"
     }
 }
